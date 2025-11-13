c4a088ceea90ffc8dcfbe67ee38de29bb6ee48bc
Brikt2 <briktsunde@gmail.com>
2025-11-13 08:23:50 +0100

    Change print statement for direction label

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def show_departures(calls):
     print("\n=== Retning sentrum ===")
     print_departures(sentrum_departures)
 
-    print("\n=== Retning gokk ===")
+    print("\n=== Den andre retningen ===")
     print_departures(other_way_departures)
 
 
